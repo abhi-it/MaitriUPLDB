51b4d9fd427d1f20692a28d758756f70a005625a
jamtechba@gmail.com <jamtechba@gmail.com>
2024-10-03 19:44:42 +0530

    Update---

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\API\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -9,7 +10,10 @@ class InventoryController extends Controller
 {
     public function index()
     {
-        $deoUsers = User::where('role_id', 5)->with('getDeoUser.zone', 'getDeoUser.division', 'getDeoUser.district', 'getDeoUser.block', 'getDeoUser.aicenter')->get();
+        $role = Role::where('name', 'deo')->first();
+        $role_id = $role ? $role->id : null;
+
+        $deoUsers = User::where('role_id', $role_id)->with('getDeoUser.zone', 'getDeoUser.division', 'getDeoUser.district', 'getDeoUser.block', 'getDeoUser.aicenter')->get();
         // dd($deoUsers);
         return view('inventory.index', compact('deoUsers'));
     }
