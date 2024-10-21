f8a913787fc890b163a1ba1e5c6a5bdb8271c15d
vijaykumarayokita <s.vijay@mufin.co.kr>
2024-10-21 16:01:25 +0530

    Add New Changes

--- resources/views/deostock/deo-stock-form.blade.php
+++ resources/views/deostock/deo-stock-form.blade.php
@@ -301,7 +301,7 @@
                     $('#select_maitri').empty();
 
                     // Check if data is returned successfully
-                    if (result.success && result.type === 'maitri' && != '') {
+                    if (result.success && result.type === 'maitri' && result.success != '') {
                         // Populate select_maitri with new options
                         $.each(result.success, function(index, maitri) {
                             // Add new options with the required value and text
