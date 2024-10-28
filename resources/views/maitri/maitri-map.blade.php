3108ab99a99372d2fa3086571a5dcfa1244706f8
vijaykumarayokita <s.vijay@mufin.co.kr>
2024-10-28 19:00:59 +0530

    New Changes

--- resources/views/maitri/maitri-map.blade.php
+++ resources/views/maitri/maitri-map.blade.php
@@ -400,6 +400,7 @@ $('#get_district').change(function() {
             success: function(data) {
 
               if(data.aicenter){
+                var code
                 $('#map').show();
                 $('#AIExport').show();
                 initAIMap(data.code, data.aicenter);
