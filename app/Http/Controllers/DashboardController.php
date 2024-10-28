6ca51f8900ce60fd06a2655089d3ed860a0dcca4
vijaykumarayokita <s.vijay@mufin.co.kr>
2024-10-28 15:22:49 +0530

    New Changes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -743,12 +743,13 @@ class DashboardController extends Controller
         if (!empty($request->input('export'))) {
             // $data = $query->select('applicationNumber', 'applicant_name', 'fname', 'mother', 'gender', 'mobile', 'email', 'high_percentage', 'inter_percentage', 'category', 'letter_address')->whereYear('created_at', $this->sessionYear)->get();
             
-            $datas = Avedan::where('is_approved', '=', 4)
-            ->join('districts', 'avedans.district_id', '=', 'districts.id') // Perform the join
-            // ->whereYear('avedans.created_at', $this->sessionYear)
-            ->where('avedans.created_at', 'LIKE', '%'.$this->sessionYear.'%')
-            ->orderBy('avedans.category', 'DESC')
-            ->get();
+            $datas = Avedan::where('is_approved', 4)
+                    ->join('districts', 'avedans.district_id', '=', 'districts.id')
+                    ->where('avedans.created_at', 'LIKE', '%'.$this->sessionYear.'%')
+                    ->orderBy('avedans.category', 'DESC')
+                    ->get();
+            
+            echo '<pre>';print_r(count($datas));exit;
             
             // $datas = $query->join('districts', 'avedans.district_id', '=', 'districts.id') // Perform the join
             //     ->whereYear('avedans.created_at', $this->sessionYear)
