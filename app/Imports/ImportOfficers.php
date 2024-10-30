58c994b07845ce7badc9f0e27e377b4ccfe40116
vijaykumarayokita <s.vijay@mufin.co.kr>
2024-10-30 16:07:18 +0530

    Add Ai Center

--- app/Imports/ImportOfficers.php
+++ app/Imports/ImportOfficers.php
@@ -18,91 +18,41 @@ class ImportOfficers implements ToModel
     {
         set_time_limit(300);
       
-        if (is_numeric($row[0])){
-            $name   =    $this->changeText($row[5]);
-            // $type   =    explode(' ',$name);
-            $block  =     $this->changeText($row[1]);
-            $checkdata = Cliniclocation::where(['name'=>$name ,'type'=>'VH'])->first();
+        if ($row[0]){
+            $mandal_name = $row[0];
+            $janpad_name = $row[1];
+            $type = $row[3];
+            $aiCenterNameHindi = $row[2];
+            $aiCenterNameEng = $row[4];
+
+            if($aiCenterNameHindi){
+                $aiCenter = $this->changeText($aiCenterNameHindi);;
+            }else{
+                $aiCenter = $this->engtohindi($aiCenterNameEng);
+            }
+            
+
+            // $name   =    $this->changeText($row[5]);
+            $checkdata = Cliniclocation::where(['mandal_name'=>$mandal_name ,'janpad_name'=> $janpad_name, 'name' => $aiCenter])->first();
             if(empty($checkdata)){
                 $maitri=new Cliniclocation();
-                $maitri->mandal_name    =    'गोरखपुर' ;//$this->engtohindi($row[2]);
-                $maitri->janpad_name    =    'देवरिया';//$this->engtohindi($row[1]);
-                $maitri->type           =    'VH';//$type[0];
-                $maitri->block          =    $block;
-                $maitri->name           =    $name;
-                $maitri->name_eng       =    $this->hinditoenglish($name);
-                $maitri->longitute      =    str_replace('-','.',$row[8]);
-                $maitri->lattitute      =    str_replace('-','.',$row[7]);
+                $maitri->mandal_name    =    $this->engtohindi($mandal_name);//$this->engtohindi($row[2]);
+                $maitri->janpad_name    =    $this->engtohindi($janpad_name);//$this->engtohindi($row[1]);
+                $maitri->type           =    $type;
+                $maitri->name           =    $aiCenter;
+                $maitri->name_eng       =    $this->hinditoenglish($aiCenter);
+                $maitri->lattitute      =    str_replace('-','.',$row[5]);
+                $maitri->longitute      =    str_replace('-','.',$row[6]);
                 $maitri->save(); 
             }else{
-                Cliniclocation::where(['name'=>$name ,'type'=>'VH'])->update([
-                    'name'              =>   $name,
-                    'name_eng'          =>   $this->hinditoenglish($name),
-                    'longitute'         =>   str_replace('-','.',$row[8]),
-                    'lattitute'         =>   str_replace('-','.',$row[7]),
+                Cliniclocation::where(['mandal_name'=>$mandal_name ,'janpad_name'=> $janpad_name, 'name' => $aiCenter])->update([
+                    'name'              =>   $aiCenter,
+                    'name_eng'          =>   $this->hinditoenglish($aiCenter),
+                    'lattitute'         =>   str_replace('-','.',$row[5]),
+                    'longitute'         =>   str_replace('-','.',$row[6]),
                 ]);
             }
         }
-
-    // if (is_numeric($row[0])) {
-        
-    //     $tehsil = isset($row[2]) && trim($row[2]) !== '' ? $this->changeText($row[2]) : $lasttehsil;
-    //     $tehsil_eng = isset($row[2]) && trim($row[2]) !== '' ? $row[2] : $this->hinditoenglish($tehsil); 
-
-        
-    //     $dis = isset($row[1]) && trim($row[1]) !== '' ? $this->engtohindi($row[1]) : $dis_id;
-    //     $dis_id = $dis;
-
-        
-    //     if (!Tehsil::where(['name_hindi' => $tehsil, 'distric' => $dis])->exists()) {
-    //         $tehsilModel = new Tehsil();
-    //         $tehsilModel->distric = $dis;
-    //         $tehsilModel->name_hindi = $tehsil;
-    //         $tehsilModel->name_eng = $tehsil_eng;
-    //         $tehsilModel->save();
-    //     }
-
-        
-    //     $block = isset($row[3]) && trim($row[3]) !== '' ? $this->engtohindi($row[3]) : $last_block;
-    //     $block_eng = isset($row[3]) && trim($row[3]) !== '' ? $row[3] : $this->hinditoenglish($block); // Use last known value if empty
-    //     $last_block = $block;
-
-        
-    //     if (!Blockslist::where(['name_hindi' => $block, 'distric' => $dis])->exists()) {
-    //         $blockModel = new Blockslist();
-    //         $blockModel->distric = $dis;
-    //         $blockModel->name_hindi = $block;
-    //         $blockModel->name_eng = $block_eng;
-    //         $blockModel->save();
-    //     }
-
-       
-        
-    //     $hos = $this->engtohindi($row[4]);
-    //     $hos_eng =$row[4];// $this->hinditoenglish($row[4]);
-    //     if (!Hospitals::where(['name_hindi' => $hos, 'distric' => $dis, 'tehsil' => $tehsil, 'block' => $block])->exists()) {
-    //         $hospitalModel = new Hospitals();
-    //         $hospitalModel->distric = $dis;
-    //         $hospitalModel->tehsil = $tehsil;
-    //         $hospitalModel->block = $block;
-    //         $hospitalModel->name_hindi = $hos;
-    //         $hospitalModel->name_eng = $hos_eng;
-    //         $hospitalModel->save();
-    //     }
-
-        
-    //     $AI = $this->engtohindi($row[5]);
-    //     $AI_eng = $row[5];//$this->hinditoenglish($AI);//
-    //     if (!AIcenters::where(['name_hindi' => $AI, 'distric' => $dis, 'tehsil' => $tehsil, 'block' => $block])->exists()) {
-    //         $aiModel = new AIcenters();
-    //         $aiModel->distric = $dis;
-    //         $aiModel->tehsil = $tehsil;
-    //         $aiModel->block = $block;
-    //         $aiModel->name_hindi = $AI;
-    //         $aiModel->name_eng = $AI_eng;
-    //         $aiModel->save();
-    //     }
-    // }
     }
 
     function changeText($val){
