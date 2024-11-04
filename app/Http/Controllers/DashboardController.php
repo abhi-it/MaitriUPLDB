ff9ea4de24df0645c055ba25e107488aa0a5f73c
vijaykumarayokita <s.vijay@mufin.co.kr>
2024-11-04 18:19:10 +0530

    New Changes for Map

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -743,24 +743,22 @@ class DashboardController extends Controller
 
         $districts = Districts::where('status', '=', 1)->orderBy('name_eng', 'ASC')->get();
 
-    
+
         $heading = 'सभी वर्गो के चयनित अभ्यर्थियों की सूची';
 
         if (!empty($request->input('export'))) {
             // $data = $query->select('applicationNumber', 'applicant_name', 'fname', 'mother', 'gender', 'mobile', 'email', 'high_percentage', 'inter_percentage', 'category', 'letter_address')->whereYear('created_at', $this->sessionYear)->get();
-            $datas = DB::table('avedans')
-                    ->join('districts', 'avedans.district_id', '=', 'districts.id')
-                    ->select('avedans.*', 'districts.*') // Select required columns
-                    ->where('avedans.is_approved', 4)
-                    ->where('avedans.created_at', 'LIKE', '%' . $this->sessionYear . '%')
-                    ->orderBy('avedans.category', 'DESC')
-                    ->get();
+            // $datas = Avedan::with('district') // Eager load the Districts model
+            //             ->where('is_approved', 4)
+            //             ->where('avedans.created_at', 'LIKE', '%' . $this->sessionYear . '%')
+            //             ->orderBy('avedans.category', 'DESC')
+            //             ->get();
             
-            // $query->join('districts', 'avedans.district_id', '=', 'districts.id') // Perform the join
-            //     ->where('avedans.created_at', 'LIKE', '%' . $this->sessionYear . '%')
-            //     ->where('avedans.is_approved', 4)
-            //     ->orderBy('avedans.category', 'DESC')
-            //     ->get();
+            $datas = $query->with('district') // Perform the join
+                ->where('avedans.created_at', 'LIKE', '%' . $this->sessionYear . '%')
+                ->where('avedans.is_approved', 4)
+                ->orderBy('avedans.category', 'DESC')
+                ->get();
 
             $data = $datas->map(function ($item) {
                 $high_percentage    = $item->high_percentage;
