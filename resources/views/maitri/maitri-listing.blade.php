f3f7cc7e6387d9a75b5fb458c7f1bc230289eb92
vijaykumarayokita <s.vijay@mufin.co.kr>
2024-11-06 12:41:13 +0530

    New Changes

--- resources/views/maitri/maitri-listing.blade.php
+++ resources/views/maitri/maitri-listing.blade.php
@@ -88,7 +88,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="editModalLabel">Edit Record</h5>
+                    <h5 class="modal-title" id="editModalLabel">Update Lat/Long</h5>
                     <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
