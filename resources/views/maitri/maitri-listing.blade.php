9d195200708b9374de5e97ff9533089316e7b7a2
vijaykumarayokita <s.vijay@mufin.co.kr>
2024-11-06 12:39:42 +0530

    New Changes

--- resources/views/maitri/maitri-listing.blade.php
+++ resources/views/maitri/maitri-listing.blade.php
@@ -89,7 +89,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="editModalLabel">Edit Record</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
@@ -134,8 +134,8 @@
                         
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn-sm btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn-sm btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
                     </div>
                 </form>
             </div>
