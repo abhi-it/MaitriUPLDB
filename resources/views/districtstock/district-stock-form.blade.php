ca1a1f9de8b691daf082e6642bf9044e85be226d
vijaykumarayokita <s.vijay@mufin.co.kr>
2024-12-16 15:30:10 +0530

    New Changes

--- resources/views/districtstock/district-stock-form.blade.php
+++ resources/views/districtstock/district-stock-form.blade.php
@@ -87,7 +87,7 @@
                         @if(count($aiCenters)>0)
                             <option value="" data-hi="AI केंद्र का चयन करें" data-en="Select AI Center"></option>
                             @foreach($aiCenters as $aiCenter)
-                                <option value="{{ $aiCenter['id'] }}" data-hi="{{ $aiCenter['name'] }}" data-en="{{ $aiCenter['name_eng'] ? $aiCenter['name_eng'] : $aiCenter['name'] }}"></option>  
+                                <option value="{{ $aiCenter['id'] }}" data-hi="{{ $aiCenter['center_name'] }}" data-en="{{ $aiCenter['center_name'] }}"></option>  
                             @endforeach
                         @else
                             <option value="" data-hi="कोई AI केंद्र नहीं" data-en="No AI Center"></option>
