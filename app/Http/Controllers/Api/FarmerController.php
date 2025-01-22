ae9fafccf8e1a1c0624c8db27cbef850a311a1c8
Jamtech <testjamtech@gmail.com>
2025-01-22 16:04:19 +0530

    New Changes

--- app/Http/Controllers/Api/FarmerController.php
+++ app/Http/Controllers/Api/FarmerController.php
@@ -49,6 +49,9 @@ class FarmerController extends Controller
             ], 401);
         }
         if ($user) {
+            $isFilled = !empty($user->name) && !empty($user->email) && !empty($user->gender) && !empty($user->pincode) && !empty($user->MobileNumber) && !empty($user->cattale_no) && !empty($user->animal_type) && !empty($user->breeds) && !empty($user->post_office) && !empty($user->block) && !empty($user->tehsil)  && !empty($user->milk_day);
+            $check_profile = $isFilled ? 'completed' : 'not_completed';
+            $user['profileDone'] = $check_profile;
             return $this->successResponse('Get User Profile Successfully',200,$user);
         }else{
             return $this->errorResponse($e->getMessage(), 500);
