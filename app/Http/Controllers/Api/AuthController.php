cbe7474a24b89545b193f5d7e72730e2336bfa19
Jamtech <testjamtech@gmail.com>
2025-01-22 11:15:29 +0530

    New Changes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -47,7 +47,7 @@ class AuthController extends Controller
                     return $this->successResponse('OTP generated successfully',200, $otp);
                 }
             } else {
-                return $this->errorResponse('Number  not correct',404);
+                return $this->errorResponse('Number  not correct',200);
             }
         } catch (\Illuminate\Validation\ValidationException $e) {
             return $this->errorResponse($e->errors(), 422);
