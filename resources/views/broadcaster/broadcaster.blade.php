6f4ba8eba163b825954f57489f1f049a09d7ed72
Jamtech <testjamtech@gmail.com>
2025-02-19 14:00:32 +0530

    New Changes

--- resources/views/broadcaster/broadcaster.blade.php
+++ resources/views/broadcaster/broadcaster.blade.php
@@ -2,73 +2,73 @@
 @section('content')
 
 <style>
-    .card-str {
-        padding: 30px;
-        max-width: 600px;
-        margin: 0 auto;
-    }
+.card-str {
+    padding: 30px;
+    max-width: 600px;
+    margin: 0 auto;
+}
 
-    .card-str p span {
-        color: #ea7327;
-    }
+.card-str p span {
+    color: #ea7327;
+}
 
-    .card-str p {
-        font-size: 18px;
-    }
+.card-str p {
+    font-size: 18px;
+}
 
-    .link-box {
-        display: flex;
-        align-items: center;
-        background: #f1f3f4;
-        padding: 12px;
-        border-radius: 8px;
-        border: 1px solid #d1d1d1;
-        width: 100%;
-        max-width: 100%;
-        margin-bottom: 20px;
-    }
+.link-box {
+    display: flex;
+    align-items: center;
+    background: #f1f3f4;
+    padding: 12px;
+    border-radius: 8px;
+    border: 1px solid #d1d1d1;
+    width: 100%;
+    max-width: 100%;
+    margin-bottom: 20px;
+}
 
-    .link-box input {
-        border: none;
-        background: transparent;
-        width: 100%;
-        font-size: 16px;
-        outline: none;
-        cursor: default;
-       
-    }
+.link-box input {
+    border: none;
+    background: transparent;
+    width: 100%;
+    font-size: 16px;
+    outline: none;
+    cursor: default;
 
-    .copy-btn {
-        background: none;
-        border: none;
-        cursor: pointer;
-        font-size: 18px;
-        margin-left: 10px;
-    }
+}
 
-    .copy-btn:hover {
-        color: #007bff;
-    }
+.copy-btn {
+    background: none;
+    border: none;
+    cursor: pointer;
+    font-size: 18px;
+    margin-left: 10px;
+}
 
-    .copy-message {
-        position: absolute;
-        top: 75px;
-        left: 84%;
-        transform: translateX(-50%);
-        background: #4caf50;
-        color: white;
-        padding: 2px 5px;
-        border-radius: 5px;
-        font-size: 14px;
-        display: none;
-        animation: fadeOut 0.2s ease-in-out 1.5s forwards;
-    }
+.copy-btn:hover {
+    color: #007bff;
+}
 
-    @keyframes fadeOut {
-        to {
-            opacity: 0;
-        }
+.copy-message {
+    position: absolute;
+    top: 75px;
+    left: 84%;
+    transform: translateX(-50%);
+    background: #4caf50;
+    color: white;
+    padding: 2px 5px;
+    border-radius: 5px;
+    font-size: 14px;
+    display: none;
+    animation: fadeOut 0.2s ease-in-out 1.5s forwards;
+}
+
+@keyframes fadeOut {
+    to {
+        opacity: 0;
     }
+}
 </style>
 
 
@@ -83,25 +83,26 @@
                     href="/join-webinar?stageArn={{ $stageArn }}">{{ url('/') }}/join-webinar?stageArn={{ $stageArn }}</a></span>
         </p>--}}
 
-            <p>Share Your Broadcasting Link</p>
-            <p>Copy this link and share it with the people.</p> 
-            <div class="copy-container">
-                <span class="copy-message" id="copy-message">Link copied!</span>
+        <p>Share Your Broadcasting Link</p>
+        <p>Copy this link and share it with the people.</p>
+        <div class="copy-container">
+            <span class="copy-message" id="copy-message">Link copied!</span>
 
-                <div class="link-box">
-                    <input type="text" id="participant-link" value="{{ route('join_webinar', ['token' => $stageArn]) }}" readonly>
-                    <button class="copy-btn" onclick="copyToClipboard()">
-                        📋
-                    </button>
-                </div>
+            <div class="link-box">
+                <input type="text" disabled id="participant-link"
+                    value="{{ route('join_webinar', ['token' => $stageArn]) }}" readonly>
+                <button class="copy-btn" disabled onclick="copyToClipboard()">
+                    📋
+                </button>
             </div>
+        </div>
 
         <form method="GET" class="text-right" target="_black"
             action="{{ route('start_webinar', ['stageArn' => $data['participantToken']['token']]) }}">
             @csrf
             <input type="hidden" name="stageArn" value="{{ $stageArn }}">
             <input type="hidden" name="token" value="{{ $data['participantToken']['token'] }}">
-            <button type="submit" class="btn btn-primary">Start Live Streaming</button>
+            <button type="submit" class="btn btn-primary" disabled>Start Live Streaming</button>
         </form>
 
     </div>
@@ -116,33 +117,33 @@
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
-    function copyToClipboard() {
-        var inputField = document.getElementById("participant-link");
-        var message = document.getElementById("copy-message");
-        // inputField.select();
-        // inputField.setSelectionRange(0, 99999); // For mobile devices
-        navigator.clipboard.writeText(inputField.value).then(() => {
-            message.style.display = "block";
-            setTimeout(() => {
-                message.style.display = "none";
-            }, 1000);
-        }).catch(err => {
-            console.error("Copy failed: ", err);
-        });
-    }
+function copyToClipboard() {
+    var inputField = document.getElementById("participant-link");
+    var message = document.getElementById("copy-message");
+    // inputField.select();
+    // inputField.setSelectionRange(0, 99999); // For mobile devices
+    navigator.clipboard.writeText(inputField.value).then(() => {
+        message.style.display = "block";
+        setTimeout(() => {
+            message.style.display = "none";
+        }, 1000);
+    }).catch(err => {
+        console.error("Copy failed: ", err);
+    });
+}
 </script>
 
 <script>
-    $(document).on("click", ".show-more", function() {
-        let fullToken = $(this).data("token");
-        $(this).parent().html(fullToken + ' <a href="javascript:void(0);" class="show-less">Show Less</a>');
-    });
+$(document).on("click", ".show-more", function() {
+    let fullToken = $(this).data("token");
+    $(this).parent().html(fullToken + ' <a href="javascript:void(0);" class="show-less">Show Less</a>');
+});
 
-    $(document).on("click", ".show-less", function() {
-        let shortToken = $(this).parent().text().substring(0, 30) + '...';
-        $(this).parent().html(shortToken + ' <a href="javascript:void(0);" class="show-more" data-token="' + $(this)
-            .parent().text() + '">Show More</a>');
-    });
+$(document).on("click", ".show-less", function() {
+    let shortToken = $(this).parent().text().substring(0, 30) + '...';
+    $(this).parent().html(shortToken + ' <a href="javascript:void(0);" class="show-more" data-token="' + $(this)
+        .parent().text() + '">Show More</a>');
+});
 </script>
 
 
