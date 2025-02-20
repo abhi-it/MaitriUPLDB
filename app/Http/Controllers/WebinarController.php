55396cd186b1a0fedefe282354c32c275e46302a
Jamtech <testjamtech@gmail.com>
2025-02-20 19:40:56 +0530

    New Changes

--- app/Http/Controllers/WebinarController.php
+++ app/Http/Controllers/WebinarController.php
@@ -184,4 +184,48 @@ class WebinarController extends Controller
         }
     }
 
+
+    public function start_webinar(Request $request)
+    {
+        try {
+            $local_arn = request()->query('localArn');
+            
+            $get_local_arn = ''; 
+            $stageArn = ''; 
+            if($local_arn){
+                $getLocalToken = Webinar::where('local_arn', 'LIKE', $local_arn)->first();
+                if($getLocalToken){
+                    $get_local_arn = $getLocalToken->local_arn;
+                    $stageArn = $getLocalToken->stage_arn;
+                }else{
+                    $stageArn = request()->query('stageArn');
+                }
+            }else{
+                $stageArn = request()->query('stageArn');
+            }
+            // $data = $this->createPublisherToken($stageArn);
+            // $token = $data['token'];
+
+            $token = $request->token ?? null;
+            $fullUrl = route('join_webinar', !empty($get_local_arn) ? [$get_local_arn] : ['token' => $stageArn]);
+            
+            $broadcastDetail = Broadcastdetails::where('stage_arn', $stageArn)->first();
+            if ($broadcastDetail) {
+                $broadcastDetail->update([
+                    'updated_at' => now(),
+                ]);
+            } else {
+                $host_id = Auth::user()->id;
+                Broadcastdetails::create([
+                    'host_id' => $host_id,
+                    'stage_arn' => $stageArn,
+                ]);
+            }
+
+            return view('broadcaster.host', compact('token', 'fullUrl','stageArn'));
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }  
+
 }
