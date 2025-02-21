77dbd84f0b175c90aaff9b8884c80464bcbb1b1b
Jamtech <testjamtech@gmail.com>
2025-02-21 13:21:35 +0530

    New Changes added in Inventory

--- app/Models/Zonestock.php
+++ app/Models/Zonestock.php
@@ -14,6 +14,7 @@ class Zonestock extends Model
         'breed_type',
         'supply_date',
         'semen' ,
+        'semen_straws',
         'semen_type' ,
         'banner' ,
         'dangler'  ,
