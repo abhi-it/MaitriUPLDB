87a206489eab5a318a904e725eb1a5ab7e84e4d3
Jamtech <testjamtech@gmail.com>
2025-04-15 13:51:00 +0530

    New Changes 12

--- resources/views/deostock/deodetails.blade.php
+++ resources/views/deostock/deodetails.blade.php
@@ -2,95 +2,96 @@
 @section('content')
 
 <style>
-    .search__button{
-        display: flex;
-        align-items: flex-end;
-        gap: 10px;
+.search__button {
+    display: flex;
+    align-items: flex-end;
+    gap: 10px;
+}
+
+@media screen and (min-width: 1024px) {
+    .table-responsive {
+        display: block !important;
     }
-    @media screen and (min-width: 1024px){
-        .table-responsive {
-            display: block !important;
-        }
-    }
+}
 </style>
-    <div x-data="" class="container main-div" style="background-color:white; height: 100%;min-height:380px;">
-        <h3 class="text-center m-4 fw-bold">  <span data-hi="वितरण स्टॉक विवरण" data-en="Distribution Stock Details"></span> </h3>
+<div x-data="" class="container main-div" style="background-color:white; height: 100%;min-height:380px;">
+    <h3 class="text-center m-4 fw-bold"> <span data-hi="वितरण स्टॉक विवरण" data-en="Distribution Stock Details"></span>
+    </h3>
 
-        <table  id="my-new-table" class="table table-striped  table-responsive table-bordered">
-            <thead>
-                <tr>
-                    <th><span data-hi="S.No" data-en="S.No"></span></th>
-                    <th><span data-hi="तरल नाइट्रोजन" data-en="Liquid Nitrogen"></span></th>
-                    <th><span data-hi="वीर्य" data-en="Semen"></span></th>
-                    <th><span data-hi="वीर्य का प्रकार" data-en="Semen Type"></span></th>
-                    <th><span data-hi="बैनर" data-en="Banner"></span></th>
-                    <th> <span data-hi="डैंगलर" data-en="Dangler"></span></th>
-                    <th><span data-hi="स्टैन्डी" data-en="Standee"></span></th>
-                    <th><span data-hi="पुस्तिका" data-en="Pamphlet"></span></th>
-                    <th> <span data-hi="एआई किट" data-en="AI Kit"></span> </th>
-                    <th> <span data-hi="पात्र" data-en="Container"></span> </th>
-                    <th> <span data-hi="कंटेनर क्षमता" data-en="Container Capacity"></span> </th>
-                    <th> <span data-hi="कायोजनार्रवाई" data-en="Scheme"></span> </th>
-                    <th> <span data-hi="बैल पहचान विवरण" data-en="Bull ID Details"></span> </th>
-                    <th> <span data-hi="निर्माण तिथि" data-en="Creation Date"></span> </th>
-                </tr>
-            </thead>
-            <tbody>
+    <table id="my-new-table" class="table table-striped  table-responsive table-bordered">
+        <thead>
+            <tr>
+                <th><span data-hi="S.No" data-en="S.No"></span></th>
+                <th><span data-hi="तरल नाइट्रोजन" data-en="Liquid Nitrogen"></span></th>
+                <th><span data-hi="वीर्य" data-en="Semen"></span></th>
+                <th><span data-hi="वीर्य का प्रकार" data-en="Semen Type"></span></th>
+                <th><span data-hi="बैनर" data-en="Banner"></span></th>
+                <th> <span data-hi="डैंगलर" data-en="Dangler"></span></th>
+                <th><span data-hi="स्टैन्डी" data-en="Standee"></span></th>
+                <th><span data-hi="पुस्तिका" data-en="Pamphlet"></span></th>
+                <th> <span data-hi="एआई किट" data-en="AI Kit"></span> </th>
+                <th> <span data-hi="पात्र" data-en="Container"></span> </th>
+                <th> <span data-hi="कंटेनर क्षमता" data-en="Container Capacity"></span> </th>
+                <th> <span data-hi="कायोजनार्रवाई" data-en="Scheme"></span> </th>
+                <th> <span data-hi="बैल पहचान विवरण" data-en="Bull ID Details"></span> </th>
+                <th> <span data-hi="निर्माण तिथि" data-en="Creation Date"></span> </th>
+            </tr>
+        </thead>
+        <tbody>
 
-                @php $i = 1; @endphp
-                @foreach ($deoStock as $key => $stockdeo)
-                    <tr>
-                        <td>{{ $i }}</td>
-                        <td>{{ $stockdeo['demand_section'] }}</td>
-                        <td>{{ $stockdeo['semen'] }}</td>
-                        <td>{{ $stockdeo['semen_type'] }}</td>
-                        <td>{{ $stockdeo['banner'] }}</td>
-                        <td>{{ $stockdeo['dangler'] }}</td>
-                        <td>{{ $stockdeo['standee'] }}</td>
-                        <td>{{ $stockdeo['pamphlet'] }}</td>
-                        <td>{{ $stockdeo['ai_kit'] }}</td>
-                        <td>{{ $stockdeo['container'] }}</td>
-                        <td>{{ $stockdeo['container_capacity'] }}</td>
-                        <td>{{ $stockdeo['scheme'] }}</td>
-                        <td>{{ $stockdeo['bull_ids'] }}</td>
-                        <td>{{ $stockdeo['created_at'] }}</td>
-                    </tr>
-                    @php $i++ @endphp
-                @endforeach
-            </tbody>
-        </table>
+            @php $i = 1; @endphp
+            @foreach ($deoStock as $key => $stockdeo)
+            <tr>
+                <td>{{ $i }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->demand_section }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->semen }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->semen_type }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->banner }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->dangler }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->standee }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->pamphlet }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->ai_kit }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->container }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->container_capacity }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->scheme }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->bull_ids }}</td>
+                <td>{{ $stockdeo->zoneStockDetails->created_at }}</td>
+            </tr>
+            @php $i++ @endphp
+            @endforeach
+        </tbody>
+    </table>
 
-        <div class="row">
+    <div class="row">
 
-        </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
-    <!-- DataTables JS -->
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.3.3/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.3.3/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.3.3/js/buttons.print.min.js"></script>
-    <!-- End DataTable -->
+</div>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+<!-- DataTables JS -->
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.3.3/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.3.3/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.3.3/js/buttons.print.min.js"></script>
+<!-- End DataTable -->
 
-    <script>
-        $(document).ready(function () {
-            $('#my-new-table').DataTable({
-                lengthMenu: [
-                    [10, 25, 50, 100, -1],
-                    [10, 25, 50, 100, "All"]
-                ],
-                pageLength: 10,
-                dom: 'lBfrtip',
-                buttons: [
-                    'csv', 'excel'
-                ]
-            });
+<script>
+$(document).ready(function() {
+    $('#my-new-table').DataTable({
+        lengthMenu: [
+            [10, 25, 50, 100, -1],
+            [10, 25, 50, 100, "All"]
+        ],
+        pageLength: 10,
+        dom: 'lBfrtip',
+        buttons: [
+            'csv', 'excel'
+        ]
+    });
 
-        });
-    </script>
+});
+</script>
 
 @endsection
-
